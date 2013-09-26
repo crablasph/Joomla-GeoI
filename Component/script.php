622bd987675e492a24344113d194edef8684c4c7
crablasph <ingenierocromeroa@gmail.com>
2013-09-26 16:10:57 -0500

    script install

--- Component/script.php
+++ Component/script.php
@@ -1,83 +1,102 @@
-<?xml version="1.0" encoding="utf-8"?>
-<extension type="component" version="2.5.0" method="upgrade">
+<?php
+// No direct access to this file
+header('Content-Type: text/html; charset=utf-8');
+defined('_JEXEC') or die('Restricted access');
  
-        <name>com_geoi</name>
-        <!-- The following elements are optional and free of formatting constraints -->
-        <creationDate>2013</creationDate>
-        <author>Carlos Romero, Jeisson Rueda</author>
-        <authorEmail>karrlyttos@hotmail.com, jeirueda@hotmail.com</authorEmail>
-        
-        <!--  The version string is recorded in the components table -->
-        <version>0.0.1</version>
-        <!-- The description is optional and defaults to the name -->
-        <!--<description>Gestor de Ofertas Inmobiliarias</description> -->
-		<description>COM_GEOI_DESC</description>
-        <scriptfile>script.php</scriptfile>
-        <install> <!-- Runs on install -->
-                <sql>
-                        <file driver="mysql" charset="utf8">sql/install.mysql.utf8.sql</file>
-                </sql>
-        </install>
-        <uninstall> <!-- Runs on uninstall -->
-                <sql>
-                        <file driver="mysql" charset="utf8">sql/uninstall.mysql.utf8.sql</file>
-                </sql>
-        </uninstall>
-        <update> <!-- Runs on update; New in 2.5 -->
-                <schemas>
-                        <schemapath type="mysql">sql/updates/mysql</schemapath>
-                </schemas>
-        </update>
-        
-        <media destination="com_geoi" folder="media">
-              <folder>css</folder>
-              <folder>js</folder>
-              <folder>images</folder>
-              <folder>openlayers</folder>
-        </media>
+/**
+ * Script file of HelloWorld component
+ */
+class com_GeoiInstallerScript
+{
+        /**
+         * method to install the component
+         *
+         * @return void
+         */
+        function install($parent) 
+        {
+        			$query ="INSERT INTO `#__geoiconf` (PARAM , VAL) VALUES";
+        			
+        			$query = $query ."
+        			('EPSG_DATA','3857') ,
+        			('EPSG_DISP','3857') ,
+        			('BOUNDS','-8279888.2058829,483769.94506356,-8203451.1776083,560206.9733381'),
+        			('MINSCALE','50000'),
+        			('ICON_1','media/com_geoi/images/building.png'),
+        			('ICON_2','media/com_geoi/images/home.png'),
+        			('ICON_3','media/com_geoi/images/shop.png'),
+        			('ICON_4','media/com_geoi/images/land.png'),
+        			('ICON_5','media/com_geoi/images/home2.png'),
+        			('ICON_6','media/com_geoi/images/home3.png'),
+        			('ICON_99','media/com_geoi/images/unknown.png'),
+        			('SEARCH_FIELDS','TYPEP:CAT,TYPEO:CAT,VALUE:INT,ROOMS:INT,TOILET:INT'),
+        			('SYMBOLOGY_FIELD','TYPEP'),
+        			('LYR_NAME','Ofertas'),
+        			('CLUSTER_DISTANCE','50'),
+        			('CLUSTER_THRESHOLD','2'),
+        			('NUMPOL','0'),
+        			('N_TYPEP','Tipo de Inmueble'),
+        			('N_TYPEO','Tipo de Oferta'),
+        			('N_VALUE','Precio'),
+        			('N_AREA','".utf8_encode('Área')."'),
+        			('N_ROOMS','".utf8_encode('Número de Habitaciones')."'),
+        			('N_AGE','Edad del Inmueble'),
+        			('N_TOILET','".utf8_encode('Número de Baños')."'),
+        			('N_TEL1','Telefono 1'),
+        			('N_TEL2','Telefono 2'),
+        			('N_EMAIL','E-mail'),
+        			('N_USERNAME','Nombre de Usuario');";
+					$db = JFactory::getDbo();
+					$db->setQuery($query);
+					$ex=$db->execute();
+					$msg=$db->getErrorMsg();
+					if (!$ex) {	echo $msg; echo "<br>";} 
+					$parent->getParent()->setRedirectURL('index.php?option=com_geoi');
+        }
  
-        <!-- Site Main File Copy Section -->
-        <!-- Note the folder attribute: This attribute describes the folder
-                to copy FROM in the package to install therefore files copied
-                in this section are copied from /site/ in the package -->
-        <files folder="site">
-                <filename>index.html</filename>
-                <filename>geoi.php</filename>
-                <filename>controller.php</filename>
-				<folder>views</folder>
-                <folder>models</folder>
-        </files>
-        <languages folder="language">
-					<language tag="en-GB">en-GB.com_geoi.ini</language>
-					<language tag="es-ES">es-ES.com_geoi.ini</language>
-		</languages>
-				
-	
-		<administration>
-                <menu>COM_GEOI_MENU</menu>
-                <submenu>
-						<menu link="option=com_geoi&amp;task=load">COM_GEOI_SUBMENU_LOAD</menu>
-						<menu link="option=com_geoi&amp;task=report">COM_GEOI_SUBMENU_REPORT</menu>
-						<menu link="option=com_geoi&amp;task=config">COM_GEOI_SUBMENU_CONFIG</menu>
-                
-				
-				</submenu>
-                <files folder="admin">
-                        <filename>index.html</filename>
-                        <filename>geoi.php</filename>
-                        <filename>controller.php</filename>
-                        <folder>sql</folder>
-						<folder>src</folder>
-                        <folder>models</folder>
-                        <folder>uploads</folder>
-                        <folder>views</folder>
-                        <folder>helpers</folder>
-                </files>
-				<languages folder="admin/language">
-					<language tag="en-GB">en-GB.com_geoi.ini</language>
-					<language tag="en-GB">en-GB.com_geoi.sys.ini</language>
-					<language tag="es-ES">es-ES.com_geoi.ini</language>
-					<language tag="es-ES">es-ES.com_geoi.sys.ini</language>
-		    	</languages>
-        </administration>
-</extension>
+        /**
+         * method to uninstall the component
+         *
+         * @return void
+         */
+        function uninstall($parent) 
+        {
+                // $parent is the class calling this method
+                echo '<p>' . JText::_('COM_HELLOWORLD_UNINSTALL_TEXT') . '</p>';
+        }
+ 
+        /**
+         * method to update the component
+         *
+         * @return void
+         */
+        function update($parent) 
+        {
+                // $parent is the class calling this method
+                echo '<p>' . JText::sprintf('COM_HELLOWORLD_UPDATE_TEXT', $parent->get('manifest')->version) . '</p>';
+        }
+ 
+        /**
+         * method to run before an install/update/uninstall method
+         *
+         * @return void
+         */
+        function preflight($type, $parent) 
+        {
+                // $parent is the class calling this method
+                // $type is the type of change (install, update or discover_install)
+                echo '<p>' . JText::_('COM_HELLOWORLD_PREFLIGHT_' . $type . '_TEXT') . '</p>';
+        }
+ 
+        /**
+         * method to run after an install/update/uninstall method
+         *
+         * @return void
+         */
+        function postflight($type, $parent) 
+        {
+                // $parent is the class calling this method
+                // $type is the type of change (install, update or discover_install)
+                echo '<p>' . JText::_('COM_HELLOWORLD_POSTFLIGHT_' . $type . '_TEXT') . '</p>';
+        }
+}
